3fc8f9a409017bb0dd013074b8e1865e7def67fa
Sahar-yahya <59683804+Sahar-yahya@users.noreply.github.com>
2021-04-26 21:46:12 +0300

    o

--- ExchangeSys/account/apps.py
+++
@@ -1,7 +0,0 @@
-"""Init app config for account app"""
-from django.apps import AppConfig
-
-
-class AccountConfig(AppConfig):
-    """Init config"""
-    name = 'account'
--- ExchangeSys/account/views.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.viewsets import ModelViewSet
-from account import serializers as account_serializers
-from account import models as account_models
-
-
-class Account(ModelViewSet):
-    queryset = account_models.Account.objects.all()
-    serializer_class = account_serializers.AccountSer
-    http_method_names = ['get']
